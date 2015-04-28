7c96ffde144fc60ffc083b03caeaac731a92ee7f
jpain <jamesepain@gmail.com>
2015-04-28 10:01:36 +0200

    Init - Base program created in 30 minutes

---
+++ Cryptonite/Crypto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Cryptonite
+{
+    static class Crypto
+    {
+        
+        static public string Encrypt(string toEncrypt, string key, bool useHashing)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = Encoding.UTF8.GetBytes(toEncrypt);
+
+            if (useHashing)
+            {
+                var hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(Encoding.UTF8.GetBytes(key));
+                hashmd5.Clear();
+            }
+            else
+                keyArray = Encoding.UTF8.GetBytes(key);
+
+            var tdes = new TripleDESCryptoServiceProvider
+            {
+                Key = keyArray,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+            var cTransform = tdes.CreateEncryptor();
+            var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            tdes.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        static public string Decrypt(string cipherString, string key, bool useHashing)
+        {
+            byte[] keyArray;
+            var toEncryptArray = Convert.FromBase64String(cipherString);
+
+            if (useHashing)
+            {
+                var hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(Encoding.UTF8.GetBytes(key));
+                hashmd5.Clear();
+            }
+            else
+                keyArray = Encoding.UTF8.GetBytes(key);
+
+            var tdes = new TripleDESCryptoServiceProvider
+            {
+                Key = keyArray,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+            var cTransform = tdes.CreateDecryptor();
+            var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            tdes.Clear();
+            return Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
---
+++ Cryptonite/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cryptonite.Properties;
+
+namespace Cryptonite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!IsFormValid()) return;
+            
+            textBox3.Text = Crypto.Encrypt(textBox2.Text, textBox1.Text, true);
+        }
+
+        private bool IsFormValid()
+        {
+            return !String.IsNullOrEmpty(textBox1.Text) &&
+                   !String.IsNullOrEmpty(textBox2.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!IsFormValid()) return;
+            try
+            {
+                Convert.FromBase64String(textBox2.Text);
+            }
+            catch (Exception)
+            {
+                textBox3.Text = Resources.Decrypt_Click_Input_string_isnt_Base64;
+                return;
+            }
+            textBox3.Text = Crypto.Decrypt(textBox2.Text, textBox1.Text, true);
+        }
+    }
+}
