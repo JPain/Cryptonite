d7a26799d98bf05be42895de1f31c81046881321
jpain <jamesepain@gmail.com>
2015-06-21 14:00:51 +0100

    Renamed Form1 to MainForm

--- Cryptonite/MainForm.Designer.cs
+++ Cryptonite/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cryptonite
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
--- Cryptonite/MainForm.cs
+++ Cryptonite/MainForm.cs
@@ -13,9 +13,9 @@ using Cryptonite.Properties;
 
 namespace Cryptonite
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
