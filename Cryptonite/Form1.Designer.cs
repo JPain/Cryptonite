540b7889d8d7e96e9705c57fe864fce39fdd77a2
jpain <jamesepain@gmail.com>
2015-06-21 13:58:11 +0100

    Added URL encoded input support. Input is now decoded before decrypting

--- Cryptonite/Form1.Designer.cs
+++ Cryptonite/Form1.Designer.cs
@@ -28,36 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.cryptKeyBox = new System.Windows.Forms.TextBox();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // cryptKeyBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(80, 14);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(229, 20);
-            this.textBox1.TabIndex = 0;
+            this.cryptKeyBox.Location = new System.Drawing.Point(80, 14);
+            this.cryptKeyBox.Name = "cryptKeyBox";
+            this.cryptKeyBox.Size = new System.Drawing.Size(229, 20);
+            this.cryptKeyBox.TabIndex = 0;
             // 
-            // textBox2
+            // inputBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(80, 40);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(229, 20);
-            this.textBox2.TabIndex = 1;
+            this.inputBox.Location = new System.Drawing.Point(80, 40);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(229, 20);
+            this.inputBox.TabIndex = 1;
             // 
-            // textBox3
+            // outputBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(80, 67);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(229, 20);
-            this.textBox3.TabIndex = 2;
+            this.outputBox.Location = new System.Drawing.Point(80, 67);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(229, 20);
+            this.outputBox.TabIndex = 2;
             // 
             // label1
             // 
@@ -87,39 +87,39 @@
             this.label3.Text = "Output";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // button1
+            // encryptButton
             // 
-            this.button1.Location = new System.Drawing.Point(153, 93);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Encrypt";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.encryptButton.Location = new System.Drawing.Point(153, 93);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(75, 23);
+            this.encryptButton.TabIndex = 6;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
             // 
-            // button2
+            // decryptButton
             // 
-            this.button2.Location = new System.Drawing.Point(234, 93);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Decrypt";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.decryptButton.Location = new System.Drawing.Point(234, 93);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(75, 23);
+            this.decryptButton.TabIndex = 7;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(332, 128);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.encryptButton);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.cryptKeyBox);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
@@ -131,14 +131,14 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox cryptKeyBox;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox outputBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button decryptButton;
     }
 }
 
--- Cryptonite/Form1.cs
+++ Cryptonite/Form1.cs
@@ -4,8 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Forms;
 using Cryptonite.Properties;
 
@@ -23,32 +25,49 @@ namespace Cryptonite
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void encryptButton_Click(object sender, EventArgs e)
         {
             if (!IsFormValid()) return;
             
-            textBox3.Text = Crypto.Encrypt(textBox2.Text, textBox1.Text, true);
+            outputBox.Text = Crypto.Encrypt(inputBox.Text, cryptKeyBox.Text, true);
         }
 
         private bool IsFormValid()
         {
-            return !String.IsNullOrEmpty(textBox1.Text) &&
-                   !String.IsNullOrEmpty(textBox2.Text);
+            return !String.IsNullOrEmpty(cryptKeyBox.Text) &&
+                   !String.IsNullOrEmpty(inputBox.Text);
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void decryptButton_Click(object sender, EventArgs e)
         {
             if (!IsFormValid()) return;
+
+            var inputEncrypted = UrlDecodeInput(inputBox.Text);
+
             try
             {
-                Convert.FromBase64String(textBox2.Text);
+                // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                Convert.FromBase64String(inputEncrypted);
             }
             catch (Exception)
             {
-                textBox3.Text = Resources.Decrypt_Click_Input_string_isnt_Base64;
+                outputBox.Text = Resources.Decrypt_Click_Input_string_isnt_Base64;
                 return;
             }
-            textBox3.Text = Crypto.Decrypt(textBox2.Text, textBox1.Text, true);
+            outputBox.Text = Crypto.Decrypt(inputEncrypted, cryptKeyBox.Text, true);
+        }
+
+        /// <summary>
+        /// Decodes an input as if it was URL encoded, leaving + chars in tact.
+        /// </summary>
+        /// <param name="text">Suspected URL encoded string</param>
+        /// <returns>Decoded string, with +</returns>
+        private static string UrlDecodeInput(string text)
+        {
+            var result = text;
+            result = result.Replace("+", "%2B");
+            result = HttpUtility.UrlDecode(result);
+            return result;
         }
     }
 }
